79822c6df65a075804136c6786880f7e13c0c608
Breno Henrique <breno.hs@outlook.com>
2024-02-14 11:01:43 -0300

    Criando api para numeros de animais para simulacao

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,27 @@
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function iniFetchAnimais() {
+  async function fetchAnimais(url) {
+    const animaisResponse = fetch(url);
+    const animaisJSON = await (await animaisResponse).json();
+    const numerosGrid = document.querySelector('.numeros-grid');
+
+    animaisJSON.forEach(animal => {
+      const divAnimal = createAnimal(animal)
+      numerosGrid.appendChild(divAnimal);
+    });
+
+    initAnimaNumeros();
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+
+    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+
+    return div;
+  }
+
+  fetchAnimais('./animaisapi.json');
+}
