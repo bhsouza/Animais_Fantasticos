2f9a1e46c5d19f7a01945eb4a5fff218a3c6f395
Breno Henrique <breno.hs@outlook.com>
2024-02-18 14:21:49 -0300

    refatora anima-numeros com class

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from './anima-numeros.js';
+import AnimaNumeros from './anima-numeros.js';
 
 export default function iniFetchAnimais() {
   async function fetchAnimais(url) {
@@ -19,8 +19,8 @@ export default function iniFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      animaNumeros.init();
     } catch (erro) {
       console.log(Error(erro));
     }
